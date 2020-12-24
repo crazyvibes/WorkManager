f6eddc0e8c62be8d3beb3e9b7aec2ce1de2ad22d
crazyvibes <bkm123r@gmail.com>
2020-12-24 22:58:30 +0530

    one time work request implemented

--- app/src/main/java/in/bk/workmanager/MainActivity.kt
+++ app/src/main/java/in/bk/workmanager/MainActivity.kt
@@ -2,10 +2,27 @@ package `in`.bk.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button=findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+
+    private fun setOneTimeWorkRequest(){
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .build()
+        WorkManager.getInstance(applicationContext).enqueue(uploadRequest)
+
     }
 }
